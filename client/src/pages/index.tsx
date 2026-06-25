745a2ca6de57a23ba931ea526e682d268d7d90e2
JillStingray1 <jerryhouau@gmail.com>
2026-06-25 18:59:05 +0800

    add news and event section to home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { Inter as FontSans } from "next/font/google";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { use, useState } from "react";
 
-import Card, { CardProp } from "@/components/ui/card";
+import { CardProp } from "@/components/ui/card";
 import CardContainer from "@/components/ui/card_container";
-import { usePings } from "@/hooks/pings";
 import { cn } from "@/lib/utils";
 import hero_image from "@/public/hero_img.png";
 import styles from "@/styles/index.module.css";
@@ -17,18 +15,38 @@ const fontSans = FontSans({
   variable: "--font-sans",
 });
 
-export default function Home() {
-  const router = useRouter();
-  const [clicked, setClicked] = useState(false);
-  const { data, isLoading } = usePings({
-    enabled: clicked,
-  });
-
-  const CardArray: Array<CardProp> = [
+/**
+ * Returns a list of upcoming events as CardProps to display
+ *
+ * @returns List of news events sorted in order
+ */
+function getEvents(): Array<CardProp> {
+  // Currently a mock function, doesn't do anything other than make some stuff to display
+  return [
     { image: hero_image, alt: "woah", title: "woah", body: "woah" },
     { image: hero_image, alt: "woah", title: "woah", body: "woah" },
     { image: hero_image, alt: "woah", title: "woah", body: "woah" },
   ];
+}
+
+/**
+ * Returns a list of most recent news as CardProps to display
+ *
+ * @returns List of news events sorted in order
+ */
+function getNews(): Array<CardProp> {
+  // Currently a mock function, doesn't do anything other than make some stuff to display
+  return [
+    { image: hero_image, alt: "woah", title: "woah", body: "woah" },
+    { image: hero_image, alt: "woah", title: "woah", body: "woah" },
+    { image: hero_image, alt: "woah", title: "woah", body: "woah" },
+  ];
+}
+
+export default function Home() {
+  const router = useRouter();
+  const events = getEvents();
+  const news = getNews();
 
   return (
     <main
@@ -43,7 +61,7 @@ export default function Home() {
             Electrifying our households to build a safer, more sustainable
             future.
           </div>
-          {/*! TODO: Replace this redirect with the actual destination page*/}
+          {/*! TODO: Replace this redirect with the actual destination page */}
           <Button
             className={styles["hero-button"]}
             onClick={() => router.push("/go-electric")}
@@ -60,11 +78,16 @@ export default function Home() {
           />
         </div>
       </div>
-      <Button onClick={() => {}}>{isLoading ? "Loading" : "Ping"}</Button>
-      <p>
-        Response from server: <span>{data as string}</span>
-      </p>
-      <CardContainer cards={CardArray} />
+      <div className={styles["news-and-events"]}>
+        <div>
+          <h1 className="text-5xl font-bold">Recent News</h1>
+          <CardContainer cards={news} />
+        </div>
+        <div>
+          <h1 className="text-5xl font-bold">Upcoming Events</h1>
+          <CardContainer cards={events} />
+        </div>
+      </div>
     </main>
   );
 }
