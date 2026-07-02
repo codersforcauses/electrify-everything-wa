f8e190abb94e01240cd470dd7cf1d78f0370087a
Joel Brooker <joeltbrooker4@gmail.com>
2026-07-02 09:15:55 +0000

    feat(resource cards): change resource cards to include design

--- client/src/pages/resources/index.tsx
+++ client/src/pages/resources/index.tsx
@@ -1,24 +1,22 @@
 import { getResources } from "@/hooks/apiService";
 
-/*
-interface Resource {
-  name: string;
-}*/
-
 function ResourcesCards() {
   const { data, isLoading } = getResources();
-  console.log("maybe loaded", data);
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading || data === undefined) return <p>Loading...</p>;
 
-  console.log("loaded", data);
-  return (
-    <ul>
-      {data.map((item, index) => (
-        <li key={index}>{item.name}</li>
-      ))}
-    </ul>
-  );
+  return data.map((item, index) => (
+    <div key={index} className="flex gap-2 shadow-md">
+      <div className="h-64 w-72 bg-gray-300"></div>
+
+      <div className="flex flex-col justify-center gap-2 px-12">
+        <p className="text-[#C81FD1]">Lebron James • 30 Jun 2026</p>
+        <h2 className="text-xl font-bold">{item.name}</h2>
+        <p>{item.summary}</p>
+        <p className="text-[#C81FD1]">View {"->"}</p>
+      </div>
+    </div>
+  ));
 }
 
 export default function Resources() {
