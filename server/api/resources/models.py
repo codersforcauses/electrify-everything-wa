cc94d404ffb5e469dbc795aab374f8fc97200306
Joel Brooker <joeltbrooker4@gmail.com>
2026-07-11 06:17:36 +0000

    feat(models): remove height field and width field and set default image to none

--- server/api/resources/models.py
+++ server/api/resources/models.py
@@ -14,7 +14,7 @@ class Resource(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     date_made = models.DateField(default="1000-10-10")
     author = models.CharField(blank=True, default="", max_length=50)
-    image = models.ImageField(null=True, upload_to="Resource", height_field=None, width_field=None, max_length=None)
+    image = models.ImageField(null=True, upload_to="Resource", max_length=None, default=None)
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
     summary = models.TextField(default="", blank=True)
